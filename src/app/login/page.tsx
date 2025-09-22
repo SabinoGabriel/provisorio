ff48034b1f39e46f0d1e981bfe5cf8bf3171180e
BruCamps <brunaevelyncampos095@gmail.com>
2025-09-21 22:08:24 -0300

    feat: Add button e topbar components

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className='w-full h-screen items-center justify-center flex flex-col space-y-16 p-10'>
+            <h1 className='text-2xl font-bold'>Login Page</h1>
+        </div>
+    )
+}
